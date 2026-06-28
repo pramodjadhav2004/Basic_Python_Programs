b3883ae331b85bb89c6db562c31269c0555231b3
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-06-28 20:04:37 +0530

    finding max min and sum in each row of a given matrix

---
+++ maxMinSumInRowsOfMatrix.py
@@ -0,0 +1,30 @@
+def print_max_min_sum_for_row_wise(num_list):
+    maxL=[]
+    minL=[]
+    sumL=[]
+    for i in num_list:
+        maxL+=[max(i)]
+        minL+=[min(i)]
+        sumL+=[sum(i)]
+    print(maxL)
+    print(minL)
+    print(sumL)
+
+def convert_string_to_int(list_a):
+    new_list = []
+    for item in list_a:
+        num = int(item)
+        new_list.append(num)
+    return new_list
+
+
+m, n = input("Enter rows and columns: ").split()
+m, n = int(m), int(n)
+num_list = []
+
+for i in range(m):
+    list_a = input("Enter Matrix: ").split()
+    list_a = convert_string_to_int(list_a)
+    num_list.append(list_a)
+
+print_max_min_sum_for_row_wise(num_list)
