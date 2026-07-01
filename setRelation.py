26f47da57d710fd3cfdb6f5759d539fc958b6c6d
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-01 22:24:34 +0530

    superset , subset, disjointset

---
+++ setRelation.py
@@ -0,0 +1,13 @@
+num_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+sub_set = input().split()
+no_list=set()
+for i in sub_set:
+    num = int(i)
+    no_list.add(num)
+if num_set.issuperset(no_list):
+    print("Superset")
+elif num_set.issubset(no_list):
+    print("Subset")
+elif num_set.isdisjoint(no_list):
+    print("Disjoint Set")
+    
