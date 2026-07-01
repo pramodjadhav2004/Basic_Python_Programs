619f642cd9f19fc18d41e26e5ef2e2cb1ba6ba84
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-01 22:25:39 +0530

    commit changes

--- setRelation.py
+++ setRelation.py
@@ -1,5 +1,5 @@
 num_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-sub_set = input().split()
+sub_set = input("Enter a list of numbers separated by spaces: ").split()
 no_list=set()
 for i in sub_set:
     num = int(i)
