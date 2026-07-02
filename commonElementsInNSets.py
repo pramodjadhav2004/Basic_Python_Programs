3e98d3075af3f96508e7cdbbd23397c87d4581fb
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-02 20:13:40 +0530

    find the common elements in N set

---
+++ commonElementsInNSets.py
@@ -0,0 +1,30 @@
+n = int(input())
+main_list = []
+def convert_int_list(num):
+    int_list = []
+    for i in num:
+        int_list += [int(i)]
+    return int_list
+
+for i in range(n):
+    num = input().split()
+    num = convert_int_list(num)
+    main_list.append(num)
+intersect = []
+for i in range(len(main_list)-1):
+    intersect = set(main_list[i]) & set(main_list[i+1])
+final_list = list(intersect)
+
+if n == 1:
+    final_list = main_list[0]
+final_list.sort()
+print(final_list)
+    
+
+
+
+
+
+
+
+    
