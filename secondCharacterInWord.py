d3664cfa645ec762bc1917a907197790e24c1be3
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-10 23:20:55 +0530

    to return and print the second character in a word using function

---
+++ secondCharacterInWord.py
@@ -0,0 +1,7 @@
+#to return and print the second character in a word using function
+def second_character(arg_1):
+    second_word = arg_1[1]
+    print(second_word)
+
+word = input("Enter a word: ")
+second_character(word)
