07c454343c5ce2433f44971069edee6dbacee518
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-15 23:53:13 +0530

    to print the character at a specific index of a word

---
+++ indexing.py
@@ -0,0 +1,7 @@
+#to print the character at a specific index of a word
+def indexing(arg_1, arg_2):
+    print(arg_1[arg_2])
+
+word = input("Enter a word: ")
+index = int(input("Enter an index: "))
+indexing(word,index)
