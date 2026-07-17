263490f7a56d6c9e383f24e378313a1ab26a6372
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-18 00:00:32 +0530

    feat: add statistical median calculator for number sequences

---
+++ medianInList.py
@@ -0,0 +1,28 @@
+"""
+Median Calculator Script
+
+This script takes a comma-separated list of numbers, sorts them, and 
+calculates the statistical median. It handles both even and odd-length 
+datasets appropriately.
+"""
+string = input()
+int_string = string.split(',')
+list_a = []
+count = len(int_string)
+
+for i in int_string:
+    list_a += [int(i)]
+sort_ace = sorted(list_a)
+if count % 2 == 0:
+    ans = count // 2
+    median = sum(sort_ace[-ans : -ans-2:-1]) / 2
+    print(median)
+else:
+    ans = count // 2
+    median = sort_ace[ans]
+    print(median)
+    
+    
+    
+    
+    
